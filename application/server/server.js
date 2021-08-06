fd7ae406d3a737d5ecb04bbdab2b3cb2bf62d143
FellPrimus <80045871+FellPrimus@users.noreply.github.com>
2021-08-06 21:43:58 +0900

    Add files via upload

--- application/server/server.js
+++ application/server/server.js
@@ -7,10 +7,14 @@ var Fabric_Client = require('fabric-client');
 var path          = require('path');
 var util          = require('util');
 var os            = require('os');
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
 var app = express();
+
 require('./controller.js')(app);
+
 app.use(express.static(path.join(__dirname, '../client')));
 var port = process.env.PORT || 8000;
 app.listen(port,function(){
